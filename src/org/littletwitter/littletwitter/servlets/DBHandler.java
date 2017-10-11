ae87777ece58d9418203479edeeb0e33bdcf2fc5
Yashasvi-Sriram <yash.3997@gmail.com>
2017-10-11 19:10:13 +0530

    adds sql files, README (explaining how to set up this server), depedency jar files

--- src/org/littletwitter/littletwitter/servlets/DBHandler.java
+++ src/org/littletwitter/littletwitter/servlets/DBHandler.java
@@ -9,8 +9,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DBHandler {
-    private static String URL = "jdbc:postgresql://localhost:5940/postgres";
-    private static String USERNAME = "joshi";
+    private static String URL = "jdbc:postgresql://localhost:5080/postgres";
+    private static String USERNAME = "pandu";
     private static String PASSWORD = "";
 
     static {
